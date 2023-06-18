3cf283298620bb3399de2b06de6346148f1fa69d
tr1ten <shubhi.blog@gmail.com>
2023-06-18 18:43:31 +0530

    feat(ui): add icons for extension store

--- src/NHeader.jsx
+++ src/NHeader.jsx
@@ -3,15 +3,14 @@ import React from 'react'
 export default function NHeader() {
   return (
     <div
-        // leetcode style header
         className='flex items-center justify-between w-full px-4 py-2 bg-white border-b border-gray-200'
     >
        <div
        className='w-full'
        >
-       <h1
-            className='text-4xl font-bold text-center text-gray-800 '
-        >Neet</h1>
+        <img
+        width='100'
+         href='logo.png' alt='logo' />
        </div>
         {/* Github icon with link */}
         <a href="#" target="_blank" rel="noopener noreferrer">
