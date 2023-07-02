52c37efecabe8de2b1f5c418269020e612062f9e
Vishal Bindal <vbindal027@gmail.com>
2023-07-02 17:10:33 +0530

    added few more algorithms in Java

--- public/templates/fenwick_tree.cpp
+++ public/templates/fenwick_tree.cpp
@@ -1,4 +1,6 @@
 const int MOD = 1e9+7;
-void update(int BIT[],int x,int val,int N) { ++x;  while(x<=N)  {  BIT[x]+=val;BIT[x] %=MOD; x+=(x&-x);  } }
+void update(int BIT[],int x,int val,int N) 
+{ ++x;  while(x<=N) 
+     {  BIT[x]+=val;BIT[x] %=MOD; x+=(x&-x);  } }
 long long query(int BIT[],int x) {  ++x;  long long res=0;  while(x>0)  {  res+=BIT[x];res%=MOD;  x-=(x&-x);  } return res; } 
 long long range(int bit[],int a,int b){ return (query(bit,b) - query(bit,a-1) + MOD)%MOD;}
