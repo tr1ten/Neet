c7863f8e999e2f1779d81693f6bf2e1f3bdffc37
tr1ten <shubhi.blog@gmail.com>
2024-03-23 19:03:24 +0530

    chore: adding new templates

--- public/templates/fast_pow.cpp
+++ public/templates/fast_pow.cpp
@@ -1,6 +1,11 @@
-long long fast_pow(long long a,long long b,long long MOD){
-    if(b==0) return 1;
-    long long res = fast_pow(a,b/2,MOD);
-    if(b%2==0) return (res*res)%MOD;
-    return (((res*res)%MOD)*a)%MOD;
+long long fast_pow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
 }
