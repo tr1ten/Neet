ab28d36f77d153c0d460fc0bf17b57b106366a22
tr1ten <shubhi.blog@gmail.com>
2024-03-30 19:26:20 +0530

    chore: adding new templates

---
+++ public/templates/modulo_arithimatic.cpp
@@ -0,0 +1,17 @@
+typedef unsigned long long ull;
+typedef long long ll;
+ull modmul(ull a, ull b, ull M) {
+	ll ret = a * b - M * ull(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
+ll modadd(ll a,ll b,ull MOD) {
+    b %= MOD;
+    a %=MOD;
+    return (a+b)%MOD;
+}
