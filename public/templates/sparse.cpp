f457a6ecd074052965a5bd6a58dd861b1debfe26
tr1ten <shubhi.blog@gmail.com>
2024-11-18 10:09:38 +0530

    improve templates

--- public/templates/sparse.cpp
+++ public/templates/sparse.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int K = 25;
-const int N = 1e5 + 5;
-
-ll st[N][K+1];
-ll merge(ll x,ll y){
-    return min(x,y);
-}
-int log2_floor(unsigned long long i) {
-    return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
-}
-void build(vector<ll> A){
-    int K = log2_floor(A.size());
-    for(int i =0;i<A.size();i++) st[i][0] = A[i];
-    for(int k=1;k<=K;k++){
-        for(int i=0;i<A.size();i++){
-            st[i][k] = merge(st[i][k-1],st[i+(1ll<<(k-1))][k-1] );
-        }
+#define all(x) x.begin(), x.end()
+template<typename T> bool ckmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+template<typename it, typename bin_op>
+struct sparse_table {
+    using T = typename remove_reference<decltype(*declval<it>())>::type;
+    vector<vector<T>> t; bin_op F;
+ 
+    sparse_table(it first, it last, bin_op op) : t(1), F(op) {
+        int n = distance(first, last);
+        t.assign(32-__builtin_clz(n), vector<T>(n));
+        t[0].assign(first, last);
+        for (int i = 1; i < t.size(); i++)
+            for (int j = 0; j < n-(1<<i)+1; j++)
+                t[i][j] = F(t[i-1][j], t[i-1][j+(1<<(i-1))]);
     }
-}
-// pre C++20
-
-ll query(int l,int r){
-    int k = log2_floor(r-l+1);
-    return merge(st[l][k],st[r-(1ll<<k) +1][k] );
-}
+ 
+    T query(int l, int r) {
+        int h = 31 - __builtin_clz(r-l+1);
+        return F(t[h][l], t[h][r-(1<<h)+1]);
+    }
+};
 int main(int argc, char const *argv[])
 {
-    vector<ll> a{2,3,1,4,5,3,1,0};
-    build(a);
-    cout << query(3,7) << endl;
-    
+    vector<int> a{2,3,1,4,5,3,1,0};
+    sparse_table st1(all(a), [&](int x, int y) { return max(x, y); }); // not sure why error
+    cout << st1.query(1,4) << endl;
     return 0;
 }
